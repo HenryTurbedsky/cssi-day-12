19b4991b17fd8cb766cf7b6f905b7f028d6160bf
HenryTurbedsky <turbo.henryg@gmail.com>
2021-07-27 17:10:28 +0000

    notes show

--- public/js/viewNotes.js
+++ public/js/viewNotes.js
@@ -2,9 +2,11 @@
 window.onload = event => {
     
     firebase.auth().onAuthStateChanged(user => {
+
         if(user)
         {
             const googleUserId = user.uid
+            getNotes(googleUserId)
         } else {
             window.location = "index.html"
         }
@@ -30,17 +32,25 @@ const renderDataAsHtml = (data) => {
     let cards =``;
     for (const noteItem in data) {
         const note = data[noteItem]
-        
+        cards += createCard(note)
     }
+    document.querySelector("#app").innerHTML = cards
 
 }
 
 
 const createCard = (note) => {
     return `
-        
-
-
+        <div class="column is-one-quarter">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">${note.title}</p>
+                </header>
+                <div class="card-content">
+                    <div class="content">${note.text}</div>
+                </div>
+            </div>
+        </div>
     `
 }
 
